04d075daad767f15fb0f069cfbbe55f86b53a71b
iambeto <rscabrera@gmail.com>
2022-12-02 21:48:00 -0600

    sidebaroptions

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import Sidebar from './Sidebar';
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
      <h1>Testing</h1>
 
      {/* Sidebar */}
+      <Sidebar />
 
      {/* Feed */}
 
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,19 +1,34 @@
 import React from 'react'
 import './Sidebar.css'
-
+import TwitterIcon from '@mui/icons-material/Twitter';
+import SidebarOption from './SidebarOption';
+import HomeIcon from '@mui/icons-material/Home';
+import SearchIcon from '@mui/icons-material/Search';
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
+import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder';
+import ListAltIcon from '@mui/icons-material/ListAlt';
+import PermIdentityIcon from '@mui/icons-material/PermIdentity';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
 
 function Sidebar() {
   return (
     <div className='sidebar'>
         {/* Twitter icon */}
+        <TwitterIcon />
 
-        {/* SidebarOption */}
-        {/* SidebarOption */}
-        {/* SidebarOption */}
-        {/* SidebarOption */}
-        {/* SidebarOption */}
-        {/* SidebarOption */}
-        {/* SidebarOption */}
+
+        <SidebarOption Icon={HomeIcon} text='Home' />
+        <SidebarOption Icon={SearchIcon} text='Explore' />
+        <SidebarOption Icon={NotificationsNoneIcon} text='Notifications' />
+        <SidebarOption Icon={MailOutlineIcon} text='Mail' />
+        <SidebarOption Icon={BookmarkBorderIcon} text='Bookmarks' />
+        <SidebarOption Icon={ListAltIcon} text='Lists' />
+        <SidebarOption Icon={PermIdentityIcon} text='Profile' />
+        <SidebarOption Icon={MoreHorizIcon} text='More' />
+
+
+
 
         {/* Button => Tweet */}
     </div>
